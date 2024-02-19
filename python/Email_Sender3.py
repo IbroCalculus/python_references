d425224f0ab13579c48244a2a3bde451ae06cd6d
isuleiman <ibsuleiman9@gmail.com>
2024-02-19 16:19:28 +0100

    Commit No.6

--- python/Email_Sender3.py
+++ python/Email_Sender3.py
@@ -1,5 +1,5 @@
 # import os
-# import win32com.client
+# import win32com.client  #(NB: pip install pywin32, FOR THIS TO WORK)
 #
 # def send_email(subject, body, to):
 #     outlook = win32com.client.Dispatch("Outlook.Application")
@@ -17,17 +17,17 @@
 #
 #     mail.Send()
 #
-# send_email("Test Subject", "Test Body", "ibsuleiman9@gmail.com")
+# send_email("Test Subject", "Test Body", "testmail@gmail.com")
 
 
 
 
 # NB: Outlook must be already installed and signed in on the computer device running this program.
 # If not sending, You can open outlook to find them in outbox. Enabling metered connection setting within solved it.
-# If display=True, this will open the outlook app, rather that just sending
+# If display=True, this will open the Outlook app, rather that just sending
 
 import os
-import win32com.client
+import win32com.client   # (NB: pip install pywin32, FOR THIS TO WORK)
 
 def send_outlook_email(subject, to, cc, body, display=False):
     ol = win32com.client.Dispatch("outlook.application")
@@ -52,4 +52,5 @@ def send_outlook_email(subject, to, cc, body, display=False):
         print(f"An error occurred: {e}")
 
 # Example usage:
-send_outlook_email('NEW SAMPLE Mail', 'example@gmail.com', 'lomih55502@oprevolt.com', 'HELLO, this is a test email.')
+# send_outlook_email('NEW SAMPLE Mail', 'example@gmail.com', 'lomih55502@oprevolt.com', 'HELLO, this is a test email.')
+send_outlook_email('NEW SAMPLE Mail', 'ibsuleiman9@gmail.com', 'lomih55502@oprevolt.com', 'HELLO, this is a test email.', True)
